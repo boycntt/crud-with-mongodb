d122071d7212283b16f71fb4d17c977dd4467345
boycntt <ttuan.net@gmail.com>
2024-07-22 10:32:26 +0700

    about api snippet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const blogRouter = require("./routes/BlogRoutes");
 const tradeRouter = require("./routes/TradeRoutes");
+const snippetRouter = require("./routes/SnippetRoutes");
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
 app.use("/api/trades", tradeRouter);
+app.use("/api/snippets", snippetRouter);
 
 //configure mongoose
 mongoose.connect(
